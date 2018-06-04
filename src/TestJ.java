e69b4ca3a6d78ceb73bf9a93b63e1fb1c79186dd
Suen <shk8000@163.com>
2018-06-04 21:28:25 +0800

    Update TestJ.java

--- src/TestJ.java
+++ src/TestJ.java
@@ -3,11 +3,7 @@
  */
 public class TestJ {
     public static void main(String[] args){
-<<<<<<< HEAD
         System.out.println("shk77877HHHHHHH11111111HRRRRRR23333");
-=======
-        System.out.println("jkl7777HHHHHHHH88888HHH23333");
->>>>>>> develop
         System.out.println("HHHHHHHHHHH22222");
     }
 }
