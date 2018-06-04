fabec8d1f048380dc9bf12263e84f7d258925c0d
Suen <shk8000@163.com>
2018-06-04 14:30:19 +0800

    Update TestJ.java
    update

--- src/TestJ.java
+++ src/TestJ.java
@@ -3,7 +3,7 @@
  */
 public class TestJ {
     public static void main(String[] args){
-        System.out.println("HHHHHHH11111111HRRRRRR23333");
+        System.out.println("888HHHHHHH11111111HRRRRRR23333");
         System.out.println("HHHHHHHHHHH22222");
     }
 }
